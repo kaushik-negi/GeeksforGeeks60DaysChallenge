ff82bb1895a435ceab6be633abcd5d2230a96c9c
kaushik-negi <155911327+kaushik-negi@users.noreply.github.com>
2026-03-20 00:06:46 +0530

    Day-30

---
+++ DistributeCandies.java
@@ -0,0 +1,10 @@
+//package geeksforgeeks;
+//
+//public class DistributeCandies {
+//    public static void main(String[] args) {
+//
+//    }
+//    static int distCandy(Node root) {
+//
+//    }
+//}
---
+++ LargestBST.java
@@ -0,0 +1,33 @@
+package geeksforgeeks;
+
+class NodeValue{
+    int maxNode;
+    int minNode;
+    int maxSize;
+
+    NodeValue(int maxNode,int minNode,int maxSize){
+        this.maxNode = maxNode;
+        this.minNode = minNode;
+        this.maxSize = maxSize;
+    }
+}
+public class LargestBST {
+    public static void main(String[] args) {
+
+    }
+    static int largestBst(Node root) {
+        return largestBstHelper(root).maxSize;
+    }
+    static NodeValue largestBstHelper(Node root){
+        if(root == null) return new NodeValue(Integer.MAX_VALUE,Integer.MIN_VALUE,0);
+
+        NodeValue left = largestBstHelper(root.left);
+        NodeValue right = largestBstHelper(root.right);
+
+        if(left.maxNode<root.data && root.data < right.minNode){
+            return new NodeValue(Math.min(root.data, left.minNode),Math.max(root.data, right.maxNode),left.maxSize+right.maxSize+1);
+        }
+
+        return new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE,Math.max(left.maxSize, right.maxSize);
+    }
+}
